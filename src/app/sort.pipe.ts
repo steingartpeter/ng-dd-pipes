7fe98eced919959981b3509b51f011049afe4c33
steingartpeter <steingart.p2@gmail.com>
2025-08-11 15:24:50 +0200

    SORT pipe, and kill 18 OnReset

---
+++ src/app/sort.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort',
+  standalone: true
+})
+export class SortPipe implements PipeTransform {
+
+  transform(value: string[] | number[], dierection:'asc'|'desc' = 'asc') {
+    const sorted = [...value];
+    sorted.sort((a,b) => {
+      if(dierection === 'asc'){
+        return a > b ? 1 :-1;
+      }else{
+        return a > b ? -1 : 1;
+      }
+    });
+    return sorted;
+  }
+
+}
